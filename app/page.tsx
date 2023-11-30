3934ab88a409590ebcadc4959f85433cc43b3c55
Josh Human <josh.human@mindflick.co.uk>
2023-11-30 16:28:19 +0000

    feat: add initial display for board

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
-import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { randomInt } from 'crypto';
 
 export default function HomePage() {
+  const create_board = (width: number, height: number): number[][] => {
+    const empty_board = Array(height).fill(Array(width).fill(0));
+    const initial_board = empty_board.map((row) => row.map((_) => randomInt(2)));
+
+    return initial_board;
+  };
+
   return (
     <>
-      <Welcome />
-      <ColorSchemeToggle />
+      <div className="w-full h-screen flex flex-col justify-center items-center">
+        {create_board(5, 5).map((row, i) => (
+            <div key={i} className="flex">
+              {row.map((item, j) => <span key={j} className={`border border-black w-16 h-16 ${item ? 'bg-black' : ''}`} />)}
+            </div>
+          ))}
+      </div>
     </>
   );
 }
